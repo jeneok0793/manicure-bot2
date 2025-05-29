8ba2dc811eef7cae61e0835eb561cc94ab4a642a
jeneok0793 <jeneok0793@gmail.com>
2025-05-29 22:11:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 # config.py
+import os
 
-BOT_TOKEN = "8167865893:AAGgIoko_RGN0G0YOhjnXMfQYy6WCosY7w8"
-GOOGLE_SHEET_ID = "16gIFFCTsBHxFaRI6uNAoQCehMey2IbGddHlcY3XxICY"
-ADMIN_CHAT_ID = 7185312775
-TIMEZONE = "Europe/Chisinau"
+BOT_TOKEN = "8167865893:AAGgIoko_RGN0G0YOhjnXMfQYy6WCosY7w8"  # Новый бот @JeneaBeautyBot
+GOOGLE_SHEET_ID = "16gIFFCTsBHxFaRI6uNAoQCehMey2IbGddHlcY3XxICY"  # Твоя таблица
+ADMIN_CHAT_ID = "7185312775"  # Телеграм ID администратора
+TIMEZONE = "Europe/Chisinau"  # Временная зона
+
+SHEET_NAMES = {
+    "clients": "clients",
+    "slots": "slots",
+    "templates": "templates",
+    "admins": "admins"
+}
