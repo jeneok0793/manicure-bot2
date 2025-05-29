5acf4e2e3902e84549589ab84d8acbf545d64d90
jeneok0793 <jeneok0793@gmail.com>
2025-05-29 20:11:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.webhook.aiohttp_server import setup_application
 from aiohttp import web
+import os
 
 from config import BOT_TOKEN
 from handlers import router
@@ -11,9 +13,15 @@ bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTM
 dp = Dispatcher()
 dp.include_router(router)
 
-app = web.Application()
-app["bot"] = bot
-setup_application(app, dp, bot=bot)
+async def on_startup(_: web.Application):
+    await bot.set_webhook("https://manicure-bot2.onrender.com")
+
+async def main():
+    app = web.Application()
+    dp.startup.register(on_startup)
+    dp.setup(app, bot=bot)
+    await setup_application(app, dp)
+    return app
 
 if __name__ == "__main__":
-    web.run_app(app, host="0.0.0.0", port=10000)
+    web.run_app(main())
