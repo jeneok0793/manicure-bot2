21f3caa5072a466c7cbfbb5b167cc73a1cc89882
jeneok0793 <jeneok0793@gmail.com>
2025-05-29 19:36:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 BOT_TOKEN = "7584326886:AAHQGcXQ_X3S6_eFLvgUsTf1mep2iRwSpms"
-WEBHOOK_URL = "https://manicure-bot2.onrender.com"
-GOOGLE_SHEET_ID = "1Sk3XGVrTgiWy9vQ-Acnog1LscsFZoU5CO5RHqxL8oLs"
+GOOGLE_SHEET_ID = "1yy9K9B0B54RvHDKbSyF3MYq_n0xSHK8Fch_NvL6ccWk"
 ADMIN_CHAT_ID = 7185312775
 TIMEZONE = "Europe/Chisinau"
-SERVICE_ACCOUNT_FILE = "service_account.json"
+
