41ac4947732e2c7fe69e787307b5addd67ba0667
jeneok0793 <jeneok0793@gmail.com>
2025-05-29 22:44:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import asyncio
+import logging
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
-
-from config import BOT_TOKEN
+from aiohttp import web
 from handlers import router
+from config import BOT_TOKEN
 
-bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+)
 dp = Dispatcher()
 dp.include_router(router)
 
+async def on_startup(bot: Bot):
+    print("Бот запущен!")
+
 async def main():
-    await dp.start_polling(bot)
+    try:
+        await bot.delete_webhook(drop_pending_updates=True)
+        await dp.start_polling(bot)
+    except Exception as e:
+        logging.exception("Ошибка при запуске бота")
 
 if __name__ == "__main__":
     asyncio.run(main())
