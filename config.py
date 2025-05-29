a54e8eed85c1c10219f73076f810aa8008209c18
jeneok0793 <jeneok0793@gmail.com>
2025-05-29 22:37:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-GOOGLE_SHEET_ID = os.getenv("GOOGLE_SHEET_ID")
-TIMEZONE = os.getenv("TIMEZONE", "Europe/Chisinau")
+BOT_TOKEN = "8167865893:AAGgIoko_RGN0G0YOhjnXMfQYy6WCosY7w8"
+GOOGLE_SHEET_ID = "16gIFFCTsBHxFaRI6uNAoQCehMey2IbGddHlCY3XxICY"
+TIMEZONE = "Europe/Chisinau"
 
 SHEET_NAMES = {
     "clients": "clients",
     "slots": "slots",
-    "admins": "admins"
+    "admins": "admins",
 }
