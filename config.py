ec53f83113e8d4a955a7d92f53517078cfc19d5e
jeneok0793 <jeneok0793@gmail.com>
2025-05-29 22:23:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,11 @@
-# config.py
 import os
 
-BOT_TOKEN = "8167865893:AAGgIoko_RGN0G0YOhjnXMfQYy6WCosY7w8"  # Новый бот @JeneaBeautyBot
-GOOGLE_SHEET_ID = "16gIFFCTsBHxFaRI6uNAoQCehMey2IbGddHlcY3XxICY"  # Твоя таблица
-ADMIN_CHAT_ID = "7185312775"  # Телеграм ID администратора
-TIMEZONE = "Europe/Chisinau"  # Временная зона
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GOOGLE_SHEET_ID = os.getenv("GOOGLE_SHEET_ID")
+TIMEZONE = os.getenv("TIMEZONE", "Europe/Chisinau")
 
 SHEET_NAMES = {
     "clients": "clients",
     "slots": "slots",
-    "templates": "templates",
     "admins": "admins"
 }
